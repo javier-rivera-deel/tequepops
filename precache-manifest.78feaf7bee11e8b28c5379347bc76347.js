3070742e068feee2fe6b4b745586773f06ab64ff
havebeeair <arq.javierriver@gmail.com>
2018-12-19 15:09:25 -0300

    Updates

---
+++ precache-manifest.78feaf7bee11e8b28c5379347bc76347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tequepops/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "44cdfa746664aef51a36",
+    "url": "/tequepops/static/js/runtime~main.44cdfa74.js"
+  },
+  {
+    "revision": "b973d9b1b527189bb434",
+    "url": "/tequepops/static/js/main.b973d9b1.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/tequepops/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "b973d9b1b527189bb434",
+    "url": "/tequepops/static/css/main.eb011c7b.chunk.css"
+  },
+  {
+    "revision": "5364876bc6e69e77e23ec9484eaf6b00",
+    "url": "/tequepops/index.html"
+  }
+];
